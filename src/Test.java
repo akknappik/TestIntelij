3baf9cc2602bc7ca84fe55e385b835130c966862
akknappik <alison.knappik@d214.org>
2018-07-12 10:36:19 -0500

    inital commit

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+public class Test {
+    public static void main(String[] args) {
+        int number = 10;
+        function("Alison");
+
+    }
+
+    public static int function(String name){
+        return 6;
+    }
+
+    public Test() {
+    }
+
+    @Override
+    public String toString() {
+        return "Test{}";
+    }
+}
